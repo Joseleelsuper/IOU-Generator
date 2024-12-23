5e804ffd87261206b4385f15e137bbff2dd8e324
José Gallardo <tory002254@gmail.com>
2024-12-23 11:56:46 +0100

    Made all Beta UI.

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ import tkinter as tk
 def main():
     ui = UI()
     root = tk.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    if screen_width == 1920 and screen_height == 1080:
-        ui.tamx, ui.tamy = 1280, 720
-    elif screen_width == 1366 and screen_height == 768:
-        ui.tamx, ui.tamy = 1024, 576
-    else:
-        ui.tamx, ui.tamy = screen_width // 2, screen_height // 2
-
     root.destroy()
     ui.run()
 
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -19,97 +19,43 @@ TAM_FONT = 16
 
 
 class UI:
-    def __init__(self, tamx: int = 1280, tamy: int = 720):
+    def __init__(self, tamx: int = 860, tamy: int = 680):
         """Initialize the UI class.
 
         Args:
-            tamx (int, optional): Screen horizontally tam. Defaults to 1280.
-            tamy (int, optional): Screen vertically tam. Defaults to 720.
+            tamx (int, optional): Screen horizontally tam. Defaults to 860.
+            tamy (int, optional): Screen vertically tam. Defaults to 680.
         """
         # Ventana principal.
         self.root = Tk()
         self.root.title("IOU Generator")
         self.root.geometry(f"{tamx}x{tamy}")
 
+        # Creamos un frame contenedor principal usando grid
+        self.main_frame = Frame(self.root)
+        self.main_frame.grid(row=0, column=0, sticky="nsew")
+
+        # Ajustamos el grid para expandirse cuando se agrande la ventana
+        self.root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
+
         # Título y autor.
-        self.label = Label(
-            self.root, text="Pagaré Generator", font=(FONT, TAM_FONT_TITLE)
-        )
-        self.label.pack(pady=PADDING_TITLES_Y, padx=PADDING_TITLES_X)
-        self.sub_label = Label(
-            self.root, text="by José Gallardo", font=(FONT, TAM_FONT)
-        )
-        self.sub_label.pack(pady=PADDING_TITLES_Y, padx=PADDING_TITLES_X)
+        self.label = Label(self.main_frame, text="Pagaré Generator", font=(FONT, TAM_FONT_TITLE))
+        self.label.grid(row=0, column=0, columnspan=3, pady=PADDING_TITLES_Y, padx=PADDING_TITLES_X)
+        self.sub_label = Label(self.main_frame, text="by José Gallardo", font=(FONT, TAM_FONT))
+        self.sub_label.grid(row=1, column=0, columnspan=3, pady=PADDING_TITLES_Y, padx=PADDING_TITLES_X)
 
         # Nombre del Prestamista
-        self.lender_label = Label(self.root, text="Prestamista:", font=(FONT, TAM_FONT))
-        self.lender_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.lender_entry = Entry(self.root, font=(FONT, TAM_FONT))
-        self.lender_entry.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
+        self.lender_label = Label(self.main_frame, text="Prestamista:", font=(FONT, TAM_FONT))
+        self.lender_label.grid(row=2, column=0, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.lender_entry = Entry(self.main_frame, font=(FONT, TAM_FONT))
+        self.lender_entry.grid(row=3, column=0, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
 
-        # Nombre del Beneficiario
-        self.debtor_label = Label(
-            self.root, text="Beneficiario:", font=(FONT, TAM_FONT)
-        )
-        self.debtor_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.debtor_entry = Entry(self.root, font=(FONT, TAM_FONT))
-        self.debtor_entry.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
-
-        # Cantidad de dinero prestada
-        self.money_label = Label(
-            self.root, text="Cantidad prestada (Ej: 50.49):", font=(FONT, TAM_FONT)
-        )
-        self.money_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.money_frame = Frame(self.root)
-        self.money_frame.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
-        self.money_entry = Entry(self.money_frame, font=(FONT, TAM_FONT))
-        self.money_entry.pack(side="left")
-        self.euro_symbol_label = Label(
-            self.money_frame, text="€", font=(FONT, TAM_FONT)
-        )
-        self.euro_symbol_label.pack(side="left")
-
-        # Fecha de devolución máxima
-        self.date_label = Label(
-            self.root,
-            text="Fecha de devolución (Ej: 31/12/2024):",
-            font=(FONT, TAM_FONT),
-        )
-        self.date_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.date_frame = Frame(self.root)
-        self.date_frame.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
-        self.date_entry = Entry(self.date_frame, font=(FONT, TAM_FONT))
-        self.date_entry.insert(
-            0, (date.today() + timedelta(days=30)).strftime("%d/%m/%Y")
-        )
-        self.date_entry.pack(side="left")
-        self.date_button = Button(
-            self.date_frame, text="📅", command=self._open_calendar
-        )
-        self.date_button.pack(side="left")
-
-        # Aumento de cantidad a devolver si no paga a tiempo
-        self.interest_label = Label(
-            self.root,
-            text="Interés por cada mes de retraso (Ej: 10%):",
-            font=(FONT, TAM_FONT),
-        )
-        self.interest_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.interest_frame = Frame(self.root)
-        self.interest_frame.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
-        self.interest_entry = Entry(self.interest_frame, font=(FONT, TAM_FONT))
-        self.interest_entry.pack(side="left")
-        self.interest_entry.insert(0, "10%")
-        self.euro_symbol_label = Label(
-            self.interest_frame, text="/mes", font=(FONT, TAM_FONT)
-        )
-        self.euro_symbol_label.pack(side="left")
-
-        # Firma del Prestamista.
-        self.lender_sign_label = Label(self.root, text="Firma del Prestamista:", font=(FONT, TAM_FONT))
-        self.lender_sign_label.place(x=tamx//2 + PADDING_X, y=150)  # Adjust y value as needed
-        self.lender_sign_frame = Frame(self.root)
-        self.lender_sign_frame.place(x=tamx//2 + PADDING_X, y=180)  # Adjust y value as needed
+        # Firma del Prestamista
+        self.lender_sign_label = Label(self.main_frame, text="Firma del Prestamista:", font=(FONT, TAM_FONT))
+        self.lender_sign_label.grid(row=2, column=2, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.lender_sign_frame = Frame(self.main_frame)
+        self.lender_sign_frame.grid(row=3, column=2, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
         self.lender_sign_entry = Entry(self.lender_sign_frame, font=(FONT, TAM_FONT))
         self.lender_sign_entry.pack(side="left")
         self.browse_sign_button = Button(self.lender_sign_frame, text="...", command=self._select_signature_file)
@@ -117,11 +63,17 @@ class UI:
         self.draw_sign_button = Button(self.lender_sign_frame, text="✏️", command=self._open_paint)
         self.draw_sign_button.pack(side="left")
 
-        # Firma del Beneficiario.
-        self.debtor_sign_label = Label(self.root, text="Firma del Beneficiario:", font=(FONT, TAM_FONT))
-        self.debtor_sign_label.place(x=tamx//2 + PADDING_X, y=300)  # Adjust y value as needed
-        self.debtor_sign_frame = Frame(self.root)
-        self.debtor_sign_frame.place(x=tamx//2 + PADDING_X, y=330)  # Adjust y value as needed
+        # Nombre del Beneficiario
+        self.debtor_label = Label(self.main_frame, text="Beneficiario:", font=(FONT, TAM_FONT))
+        self.debtor_label.grid(row=4, column=0, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.debtor_entry = Entry(self.main_frame, font=(FONT, TAM_FONT))
+        self.debtor_entry.grid(row=5, column=0, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
+
+        # Firma del Beneficiario
+        self.debtor_sign_label = Label(self.main_frame, text="Firma del Beneficiario:", font=(FONT, TAM_FONT))
+        self.debtor_sign_label.grid(row=4, column=2, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.debtor_sign_frame = Frame(self.main_frame)
+        self.debtor_sign_frame.grid(row=5, column=2, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
         self.debtor_sign_entry = Entry(self.debtor_sign_frame, font=(FONT, TAM_FONT))
         self.debtor_sign_entry.pack(side="left")
         self.browse_sign_button = Button(self.debtor_sign_frame, text="...", command=self._select_signature_file)
@@ -129,13 +81,50 @@ class UI:
         self.draw_sign_button = Button(self.debtor_sign_frame, text="✏️", command=self._open_paint)
         self.draw_sign_button.pack(side="left")
 
+        # Cantidad de dinero prestada
+        self.money_label = Label(self.main_frame, text="Cantidad prestada (Ej: 50.49):", font=(FONT, TAM_FONT))
+        self.money_label.grid(row=6, column=0, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.money_frame = Frame(self.main_frame)
+        self.money_frame.grid(row=7, column=0, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
+        self.money_entry = Entry(self.money_frame, font=(FONT, TAM_FONT))
+        self.money_entry.pack(side="left")
+        self.euro_symbol_label = Label(self.money_frame, text="€", font=(FONT, TAM_FONT))
+        self.euro_symbol_label.pack(side="left")
+
+        # Fecha de devolución máxima
+        self.date_label = Label(self.main_frame, text="Fecha de devolución (Ej: 31/12/2024):", font=(FONT, TAM_FONT))
+        self.date_label.grid(row=8, column=0, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.date_frame = Frame(self.main_frame)
+        self.date_frame.grid(row=9, column=0, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
+        self.date_entry = Entry(self.date_frame, font=(FONT, TAM_FONT))
+        self.date_entry.insert(0, (date.today() + timedelta(days=30)).strftime("%d/%m/%Y"))
+        self.date_entry.pack(side="left")
+        self.date_button = Button(self.date_frame, text="📅", command=self._open_calendar)
+        self.date_button.pack(side="left")
+
+        # Aumento de cantidad a devolver si no paga a tiempo
+        self.interest_label = Label(self.main_frame, text="Interés por cada mes de retraso (Ej: 10%):", font=(FONT, TAM_FONT))
+        self.interest_label.grid(row=10, column=0, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.interest_frame = Frame(self.main_frame)
+        self.interest_frame.grid(row=11, column=0, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
+        self.interest_entry = Entry(self.interest_frame, font=(FONT, TAM_FONT))
+        self.interest_entry.pack(side="left")
+        self.interest_entry.insert(0, "10%")
+        self.euro_symbol_label = Label(self.interest_frame, text="/mes", font=(FONT, TAM_FONT))
+        self.euro_symbol_label.pack(side="left")
+
         # Lugar de devolución
-        self.place_label = Label(
-            self.root, text="Lugar de devolución:", font=(FONT, TAM_FONT)
-        )
-        self.place_label.pack(anchor="w", pady=PADDING_LABELS_Y, padx=PADDING_X)
-        self.place_entry = Entry(self.root, font=(FONT, TAM_FONT))
-        self.place_entry.pack(anchor="w", pady=PADDING_ENTRYS_Y, padx=PADDING_X)
+        self.place_label = Label(self.main_frame, text="Lugar de devolución:", font=(FONT, TAM_FONT))
+        self.place_label.grid(row=6, column=2, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="w")
+        self.place_entry = Entry(self.main_frame, font=(FONT, TAM_FONT))
+        self.place_entry.grid(row=7, column=2, padx=PADDING_X, pady=PADDING_ENTRYS_Y, sticky="w")
+
+        # Botón para generar el pagaré en un PDF
+        self.generate_button = Button(self.main_frame, text="Generar Pagaré", font=(FONT, TAM_FONT), command=self._generate_iou)
+        self.generate_button.grid(row=10, column=2, padx=PADDING_X, pady=PADDING_LABELS_Y, sticky="n")
+
+    def _generate_iou(self):
+        pass
 
     def _open_calendar(self):
         """Open a calendar to select a date."""
